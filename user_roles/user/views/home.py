58d4c3bffd8e5fbc5d59f0e9397f8de5ed83f03b
Gaurav Goyal <gaurav1997dlp@gmail.com>
2020-09-02 15:43:32 +0530

    home page up after login

--- user_roles/user/views/home.py
+++ user_roles/user/views/home.py
@@ -4,9 +4,10 @@ Purpose:- Home page define
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from rolepermissions.roles import get_user_roles
 
 
 
-#@login_required
+@login_required
 def home(request):
 	return HttpResponse('home page')

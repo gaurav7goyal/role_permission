b534535150b372769853f18ab24cf2f4eed4680d
Gaurav Goyal <gaurav1997dlp@gmail.com>
2020-09-02 19:24:39 +0530

    user role add

--- user_roles/user/roles.py
+++
@@ -1,9 +0,0 @@
-'''
-user custom role define
-'''
-from rolepermissions.roles import AbstractUserRole
-
-class Doctor(AbstractUserRole):
-    available_permissions = {
-        'create_medical_record': True,
-    }
